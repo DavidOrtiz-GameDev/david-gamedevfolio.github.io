429e4bb03c272206dfaa76cb1cbf1fdb605a6376
David GameDev <79913159+DavidOrtiz-GameDev@users.noreply.github.com>
2026-02-23 15:46:07 +0100

    Content update

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -1,18 +1,18 @@
 export const PERSONAL_INFO = {
-    name: "Jhon Doe",
+    name: "David Ortiz",
     title: "Who stands behind the code?",
-    description: "Hi! I'm Jhon Doe, a Full Stack Developer with a passion for building digital experiences that marry form and function. With over 3 years of experience, I've specialized in creating robust architectures and fluid user interfaces.",
-    journey: "My journey started with a curiosity for how things work under the hood, leading me to master modern technologies like Astro, React, and Node.js.",
+    description: "Hi! I'm David Ortiz, a video game developer who enjoy learning new technologies, taking on creative challenges, and contributing to experiences that resonate with players. My background combines technical experience, creativity, and a strong understanding of the development process, allowing me to contribute effectively to both game production and technical workflows.",
+    journey: "In my most recent stage, I have been developing my own video game, World of Classes, from the ground up, working on programming, system design, internal tools, resource management, and graphical implementation.",
     skills: ["🚀 Fast Learner", "💡 Problem Solver", "🤝 Team Player"],
-    heroDescription: "I'm a passionate developer with a strong background in web development and a deep love for creating innovative and user-friendly applications.",
+    heroDescription: "I'm an indie video game developer highly motivated to continue growing in the video game industry.",
     resumeUrl: "/",
     socialLinks: [
         {
-            href: "https://www.linkedin.com/",
+            href: "https://www.linkedin.com/in/david-ortiz-aguilera/",
             label: "LinkedIn",
         },
-        { href: "https://github.com/", label: "GitHub" },
+        { href: "https://github.com/DavidOrtiz-GameDev", label: "GitHub" },
         { href: "https://x.com/", label: "Twitter" },
-        { href: "mailto:example@gmail.com", label: "Email" },
+        { href: "mailto:d.ortiz.aguilera@gmail.com", label: "Email" },
     ],
 };
